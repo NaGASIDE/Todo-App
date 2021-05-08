d2723a07f9f6f5043c46d61d0667522c82973091
Bekzat Ashke <bekzatashken@gmail.com>
2021-05-08 21:12:49 +0600

    fix: UserPanel icons

--- src/components/Header/UserPanel/UserPanel.jsx
+++ src/components/Header/UserPanel/UserPanel.jsx
@@ -1,16 +1,23 @@
 import React, { useState } from 'react';
 import { Modal } from '../../../utilities/CreateModal/Modal'
+import { FiSettings } from 'react-icons/fi'
+import { IoMdHelp } from 'react-icons/io'
+import { AiOutlineUser } from 'react-icons/ai'
 import './style.sass'
 
 export function UserPanel({className}) {
 
   const [isOpen, setIsOpen] = useState(false)
-
+  
   return ( 
     <>
-      <div className={`icon ${className}`} onClick={() => setIsOpen(true)} />
-      <Modal open={isOpen} onClose={() => setIsOpen(false)} >
-        Fence Color
+      <div className={`icon`} onClick={() => setIsOpen(true)}>
+        {
+          className === `settings` ? <FiSettings/> : 
+           className === `help` ? <IoMdHelp/> : <AiOutlineUser />
+        }
+      </div> 
+      <Modal open={isOpen} onClose={() => setIsOpen(false)}>
       </Modal>
     </>
   )
