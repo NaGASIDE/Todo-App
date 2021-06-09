4740dd99bd85af9cfd7caa1369d825ba26832d37
Bekzat Ashke <bekzatashken@gmail.com>
2021-06-09 16:57:03 +0600

    feat: gh-pages

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import { App } from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const root = document.getElementById("root");
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   root
 );
